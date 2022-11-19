110ce7219c545984f863713630b79030be11b290
taeheeL <t9h9@naver.com>
2022-11-20 02:10:52 +0900

    [feat] ProfileActivity 초안 완료

---
+++ app/src/main/java/com/junewon/soptkathon/presentation/profile/BadgeAdapter.kt
@@ -0,0 +1,49 @@
+package com.junewon.soptkathon.presentation.profile
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import com.junewon.soptkathon.databinding.ItemBadgeBinding
+
+class BadgeAdapter(context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val inflater by lazy { LayoutInflater.from(context) }
+    private var badgeList: List<Badge> = emptyList()
+
+    class BadgeViewHolder(
+        private var binding: ItemBadgeBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: Badge) {
+            if (!data.isLocked) {
+                binding.ivBadge.load(data.image){
+                    transformations(RoundedCornersTransformation(28f))
+                    crossfade(true)
+                }
+                binding.ivBadge.setImageResource(data.image)
+            }
+            binding.tvBadge.text = data.name
+
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return BadgeViewHolder(ItemBadgeBinding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is BadgeViewHolder -> holder.onBind(badgeList[position])
+            else -> throw IllegalArgumentException("on BadgeViewHolder")
+        }
+    }
+
+    override fun getItemCount() = badgeList.size
+
+    fun setBadgeList(badgeList: List<Badge>){
+        this.badgeList = badgeList.toList()
+        notifyDataSetChanged()
+    }
+}
