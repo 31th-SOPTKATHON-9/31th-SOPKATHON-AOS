83603a1ae6cdcb7c6c5e973c3541ffbb349c42f6
Daeun <eldms0406@naver.com>
2022-11-20 06:30:41 +0900

    Home Activity 2차 수정(서버통신x)

--- app/src/main/java/com/junewon/soptkathon/presentation/home/HabitData.kt
+++ app/src/main/java/com/junewon/soptkathon/presentation/home/HabitData.kt
@@ -1,7 +1,10 @@
 package com.junewon.soptkathon.presentation.home
 
 data class HabitData(
-    val habit1: String,
-    val habit2: String,
-    val habit3: String
+    val habit1title: String,
+    val habit1desc: String,
+    val habit2title: String,
+    val habit2desc: String,
+    val habit3title: String,
+    val habit3desc: String,
 )
--- app/src/main/java/com/junewon/soptkathon/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/junewon/soptkathon/presentation/home/HomeActivity.kt
@@ -3,24 +3,35 @@ package com.junewon.soptkathon.presentation.home
 import android.os.Bundle
 import androidx.core.content.ContentProviderCompat.requireContext
 import com.junewon.soptkathon.R
+import com.junewon.soptkathon.data.service.SpangService
 import com.junewon.soptkathon.databinding.ActivityHomeBinding
 import com.junewon.soptkathon.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
-
+    @Inject
+    lateinit var service: SpangService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // do something..
         val list = arrayListOf<HabitData>(
-            HabitData("습관11", "습관22","습관33"),
-            HabitData("습관11", "습관22","습관33"),
-            HabitData("습관11", "습관22","습관33"),
-            HabitData("습관11", "습관22","습관33"),
-            HabitData("습관11", "습관22","습관33"),
-            HabitData("습관11", "습관22","습관33"),
-            HabitData("습관11", "습관22","습관33")
+            HabitData("습관 제목1", "아침 10분 독서","습관 제목2",
+                "1시간 운동하기", "습관 제목 3", "아침먹기"),
+            HabitData("습관 제목1", "아침 10분 독서","습관 제목2",
+                "1시간 운동하기", "습관 제목 3", "아침먹기"),
+            HabitData("습관 제목1", "아침 10분 독서","습관 제목2",
+                "1시간 운동하기", "습관 제목 3", "아침먹기"),
+            HabitData("습관 제목1", "아침 10분 독서","습관 제목2",
+                "1시간 운동하기", "습관 제목 3", "아침먹기"),
+            HabitData("습관 제목1", "아침 10분 독서","습관 제목2",
+                "1시간 운동하기", "습관 제목 3", "아침먹기"),
+            HabitData("습관 제목1", "아침 10분 독서","습관 제목2",
+                "1시간 운동하기", "습관 제목 3", "아침먹기"),
+            HabitData("습관 제목1", "아침 10분 독서","습관 제목2",
+                "1시간 운동하기", "습관 제목 3", "아침먹기")
+
         )
 
         val adapter = HomeAdapter(this)
