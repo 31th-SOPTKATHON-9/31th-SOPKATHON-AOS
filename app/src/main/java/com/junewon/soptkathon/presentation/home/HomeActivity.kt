70b604dcd1b96554fc92d63c29a3d235e4099ff3
murjune <sis9221302@gmail.com>
2022-11-20 02:51:23 +0900

    바텀 시트 구현 끝

--- app/src/main/java/com/junewon/soptkathon/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/junewon/soptkathon/presentation/home/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.junewon.soptkathon.presentation.home
 import android.os.Bundle
 import com.junewon.soptkathon.R
 import com.junewon.soptkathon.databinding.ActivityHomeBinding
+import com.junewon.soptkathon.presentation.profile.BadgeBottomSheetDialog
 import com.junewon.soptkathon.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,5 +13,12 @@ class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // do something..
+        binding.tvHome.setOnClickListener {
+            showBottomSheet()
+        }
+    }
+
+    private fun showBottomSheet() {
+        BadgeBottomSheetDialog().show(supportFragmentManager, "tag")
     }
 }
